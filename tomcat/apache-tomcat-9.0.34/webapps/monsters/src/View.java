3bbf213c19c9a72ea39df6eb6ed7d64a8390c333
tama <sha7tama@gmail.com>
2020-05-29 17:36:17 +0900

    ディレクトリ名変更

---
+++ tomcat/apache-tomcat-9.0.34/webapps/monsters/src/View.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class View extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+            String view = "/WEB-INF/views/index.jsp";
+            RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+            dispatcher.forward(request, response);
+    }
+}
